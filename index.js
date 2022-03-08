eef089cf4311eb897f0a1875b5cf2af04894f1cf
alcioneufv <95593589+alcioneufv@users.noreply.github.com>
2022-03-08 14:27:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
